b83f5c3dcce1d6df958b62265575933bcc0155b3
canicemichael <canicemichael@gmail.com>
2022-03-18 17:00:15 +0100

    Update sidebar updated

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import './sidebar.css';
-import { RssFeed } from "@material-ui/icons";
+import { Bookmark, Chat, Event, Group, HelpOutline, PlayCircleFilledOutlined, RssFeed, School, WorkOutline } from "@material-ui/icons";
 
 export default function Sidebar() {
   return (
@@ -10,10 +10,82 @@ export default function Sidebar() {
               <RssFeed className='sidebarIcon'/>
               <span className="sidebarListItemText">Feed</span>
             </li>
+            <li className="sidebarListItem">
+              <Chat className='sidebarIcon'/>
+              <span className="sidebarListItemText">Chats</span>
+            </li>
+            <li className="sidebarListItem">
+              <PlayCircleFilledOutlined className='sidebarIcon'/>
+              <span className="sidebarListItemText">Videos</span>
+            </li>
+            <li className="sidebarListItem">
+              <Group className='sidebarIcon'/>
+              <span className="sidebarListItemText">Groups</span>
+            </li>
+            <li className="sidebarListItem">
+              <Bookmark className='sidebarIcon'/>
+              <span className="sidebarListItemText">Bookmarks</span>
+            </li>
+            <li className="sidebarListItem">
+              <HelpOutline className='sidebarIcon'/>
+              <span className="sidebarListItemText">Questions</span>
+            </li>
+            <li className="sidebarListItem">
+              <WorkOutline className='sidebarIcon'/>
+              <span className="sidebarListItemText">Jobs</span>
+            </li>
+            <li className="sidebarListItem">
+              <Event className='sidebarIcon'/>
+              <span className="sidebarListItemText">Events</span>
+            </li>
+            <li className="sidebarListItem">
+              <School className='sidebarIcon'/>
+              <span className="sidebarListItemText">Courses</span>
+            </li>
           </ul>
           <button className='sidebarButton'>Show More</button>
           <hr className='sidebarHr' />
           <ul className="sidebarFriendList">
+            <li className='sidebarFriend'>
+              <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
+              <span className="sidebarFriendName">James Bond</span>
+            </li>
+            <li className='sidebarFriend'>
+              <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
+              <span className="sidebarFriendName">James Bond</span>
+            </li>
+            <li className='sidebarFriend'>
+              <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
+              <span className="sidebarFriendName">James Bond</span>
+            </li>
+            <li className='sidebarFriend'>
+              <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
+              <span className="sidebarFriendName">James Bond</span>
+            </li>
+            <li className='sidebarFriend'>
+              <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
+              <span className="sidebarFriendName">James Bond</span>
+            </li>
+            <li className='sidebarFriend'>
+              <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
+              <span className="sidebarFriendName">James Bond</span>
+            </li>
+            <li className='sidebarFriend'>
+              <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
+              <span className="sidebarFriendName">James Bond</span>
+            </li>
+            <li className='sidebarFriend'>
+              <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
+              <span className="sidebarFriendName">James Bond</span>
+            </li>
+            <li className='sidebarFriend'>
+              <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
+              <span className="sidebarFriendName">James Bond</span>
+            </li>
+            <li className='sidebarFriend'>
+              <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
+              <span className="sidebarFriendName">James Bond</span>
+            </li>
             <li className='sidebarFriend'>
               <img src="/assets/persons/2.jpg" alt="apicture" className="sidebarFriendImg" />
               <span className="sidebarFriendName">James Bond</span>
