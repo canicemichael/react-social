79f123c30905db4f73fa116551639734d4b730ab
canicemichael <canicemichael@gmail.com>
2022-03-19 13:39:14 +0100

    Add added a dummy data to fill in some required data will update it api requests later on

---
+++ src/dummyData.js
@@ -0,0 +1,145 @@
+export const Users = [
+    {
+        id:1,
+        profilePicture: "assets/persons/1.jpg",
+        username: "Donf Tykdj"
+    },
+    {
+        id:2,
+        profilePicture: "assets/persons/2.jpg",
+        username: "Donf Tykdj"
+    },
+    {
+        id:3,
+        profilePicture: "assets/persons/3.jpg",
+        username: "Donf Tykdj"
+    },
+    {
+        id:4,
+        profilePicture: "assets/persons/4.jpg",
+        username: "Donf Tykdj"
+    },
+    {
+        id:5,
+        profilePicture: "assets/persons/5.jpg",
+        username: "Donf Tykdj"
+    },
+    {
+        id:6,
+        profilePicture: "assets/persons/6.jpg",
+        username: "Donf Tykdj"
+    },
+    {
+        id:7,
+        profilePicture: "assets/persons/7.jpg",
+        username: "Donf Tykdj"
+    },
+    {
+        id:8,
+        profilePicture: "assets/persons/8.jpg",
+        username: "Donf Tykdj"
+    },
+    {
+        id:9,
+        profilePicture: "assets/persons/9.jpg",
+        username: "Fugfh Diosnd"
+    },
+    {
+        id:10,
+        profilePicture: "assets/persons/10.jpg",
+        username: "Koffe Hudgf"
+    },
+];
+
+export const Posts = [
+    {
+        id: 1,
+        desc: "Love For All, Hatred For None",
+        photo: "assets/post/1.jpg",
+        date: "5 mins ago",
+        userId: 1,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id: 2,
+        desc: "Love For All, Hatred For None",
+        photo: "assets/post/2.jpg",
+        date: "15 mins ago",
+        userId: 2,
+        like: 3,
+        comment: 1,
+    },
+    {
+        id: 3,
+        desc: "Love For All, Hatred For None",
+        photo: "assets/post/3.jpg",
+        date: "12 mins ago",
+        userId: 3,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id: 4,
+        desc: "Love For All, Hatred For None",
+        photo: "assets/post/4.jpg",
+        date: "12 mins ago",
+        userId: 4,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id: 5,
+        desc: "Love For All, Hatred For None",
+        photo: "assets/post/5.jpg",
+        date: "12 mins ago",
+        userId: 5,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id: 6,
+        desc: "Love For All, Hatred For None",
+        photo: "assets/post/1.jpg",
+        date: "12 mins ago",
+        userId: 6,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id: 7,
+        desc: "Love For All, Hatred For None",
+        photo: "assets/post/2.jpg",
+        date: "12 mins ago",
+        userId: 7,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id: 8,
+        desc: "Love For All, Hatred For None",
+        photo: "assets/post/3.jpg",
+        date: "12 mins ago",
+        userId: 8,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id: 9,
+        desc: "Love For All, Hatred For None",
+        photo: "assets/post/4.jpg",
+        date: "12 mins ago",
+        userId: 9,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id: 10,
+        desc: "Love For All, Hatred For None",
+        photo: "assets/post/5.jpg",
+        date: "12 mins ago",
+        userId: 10,
+        like: 32,
+        comment: 9,
+    },
+]
