20ce5367251aa2d7701b0af75b103d48ba883768
canicemichael <canicemichael@gmail.com>
2022-03-20 12:49:35 +0100

    Add initiated the login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import './login.css'
+
+export default function Login() {
+  return (
+    <div className='login'>
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className='loginLogo'>Tigisocial</h3>
+                <span className="loginDesc">
+                    Connect with friends and the world around you on Tigisocial
+                </span>
+            </div>
+            <div className="loginRight"></div>
+        </div>
+    </div>
+  )
+}
