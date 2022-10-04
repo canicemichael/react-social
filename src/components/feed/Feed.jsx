8c6a72f880e200f60fa18ce6b51cc846a0cc89e3
canicemichael <canicemichael@gmail.com>
2022-10-04 09:56:28 +0100

    update fetching post api

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,18 +1,30 @@
 import Share from "../share/Share";
 import Post from "../post/Post";
-import './feed.css';
-import { Posts } from "../../dummyData";
+import "./feed.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts/timeline/633ad4f4192e689a72794164");
+      setPosts(res.data);
+      // console.log(res);
+    };
+    fetchPosts();
+  }, []);
+
   return (
-    <div className='feed'>
-        <div className="feedWrapper">
-          <Share />
-          
-          {Posts.map((p) => (
-            <Post key={p.id} post={p} />
-          ))}
-        </div>
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+
+        {posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,12 +1,22 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
-import { Users } from "../../dummyData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { format } from "timeago.js";
 
 export default function Post({ post }) {
-  const [like, setLike] = useState(post.like);
+  const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState([]);
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`users/${post.userId}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, []);
 
   const likeHandler = () => {
     setLike(isLiked ? like - 1 : like + 1);
@@ -18,14 +28,12 @@ export default function Post({ post }) {
         <div className="postTop">
           <div className="postTopLeft">
             <img
-              src={Users.filter((u) => u.id === post.userId)[0].profilePicture}
+              src={user.profilePicture || PF + "persons/no-avatar.jpg"}
               alt=""
               className="postProfileImg"
             />
-            <span className="postUsername">
-              {Users.filter((u) => u.id === post.userId)[0].username}
-            </span>
-            <span className="postDate">{post.date}</span>
+            <span className="postUsername">{post?.userId || "ebuka"}</span>
+            <span className="postDate">{format(post.createdAt)}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
@@ -33,7 +41,7 @@ export default function Post({ post }) {
         </div>
         <div className="postCenter">
           <span className="postText">{post?.desc}</span>
-          <img className="postImg" src={PF + post.photo} alt="" />
+          <img className="postImg" src={post?.img} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
